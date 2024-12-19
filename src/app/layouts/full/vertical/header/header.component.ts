c5ceed4b8c56cd84fb6ca448349e990a977aca38
cesar <clgrch@gmail.com>
2024-12-19 14:36:18 -0300

    Primer commit

---
+++ src/app/layouts/full/vertical/header/header.component.ts
@@ -0,0 +1,77 @@
+import {
+  Component,
+  Output,
+  EventEmitter,
+  Input,
+  ViewEncapsulation,
+} from '@angular/core';
+import { CoreService } from 'src/app/services/core.service';
+import { MatDialog } from '@angular/material/dialog';
+import { navItems } from '../sidebar/sidebar-data';
+import { TranslateService } from '@ngx-translate/core';
+import { TablerIconsModule } from 'angular-tabler-icons';
+import { MaterialModule } from 'src/app/material.module';
+import { RouterModule } from '@angular/router';
+import { CommonModule, NgForOf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+
+
+interface profiledd {
+  id: number;
+  img: string;
+  title: string;
+  subtitle: string;
+  link: string;
+}
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [RouterModule, CommonModule, NgScrollbarModule, TablerIconsModule, MaterialModule],
+  templateUrl: './header.component.html',
+  encapsulation: ViewEncapsulation.None,
+})
+export class HeaderComponent {
+  @Input() showToggle = true;
+  @Input() toggleChecked = false;
+  @Output() toggleMobileNav = new EventEmitter<void>();
+  @Output() toggleMobileFilterNav = new EventEmitter<void>();
+  @Output() toggleCollapsed = new EventEmitter<void>();
+
+  showFiller = false;
+
+  public selectedLanguage: any = {
+    language: 'English',
+    code: 'en',
+    type: 'US',
+    icon: '/assets/images/flag/icon-flag-en.svg',
+  };
+
+
+  constructor(
+    private vsidenav: CoreService,
+    public dialog: MatDialog,
+    private translate: TranslateService
+  ) {
+    translate.setDefaultLang('en');
+  }
+
+  changeLanguage(lang: any): void {
+    this.translate.use(lang.code);
+    this.selectedLanguage = lang;
+  }
+
+  profiledd: profiledd[] = [
+    {
+      id: 1,
+      img: '/assets/images/svgs/icon-account.svg',
+      title: 'Mi Perfil',
+      subtitle: 'Configuración',
+      link: '/',
+    },
+  ];
+
+}
+
+
---
+++ src/app/layouts/full/vertical/sidebar/sidebar-data.ts
@@ -0,0 +1,60 @@
+import { NavItem } from './nav-item/nav-item';
+
+export const navItems: NavItem[] = [
+  {
+    navCap: 'Menu',
+  },
+  {
+    displayName: 'Dashboard',
+    iconName: 'layout-dashboard',
+  },
+  {
+    displayName: 'Despacho',
+    iconName: 'truck',
+    route: '/',
+    children: [
+      {
+        displayName: 'Control Despacho',
+        iconName: 'point',
+        route: '/',
+      },
+      {
+        displayName: 'Mapa',
+        iconName: 'point',
+        route: '/',
+      },
+    ],
+  },
+  {
+    displayName: 'Planificador',
+    iconName: 'calendar-week',
+    route: '/authentication/register',
+  },
+  {
+    displayName: 'Reportabilidad',
+    iconName: 'clipboard-data',
+    route: '/authentication/register',
+  },
+  {
+    displayName: 'Auditoria',
+    iconName: 'eye-edit',
+    route: '/authentication/register',
+  },
+  {
+    displayName: 'Configuración',
+    iconName: 'settings-cog',
+    route: '/',
+    children: [
+      {
+        displayName: 'Flota',
+        iconName: 'point',
+        route: '/',
+      },
+      {
+        displayName: 'Parametros',
+        iconName: 'point',
+        route: '/',
+      },
+    ]
+  },
+];
